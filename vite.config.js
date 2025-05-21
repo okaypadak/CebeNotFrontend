b148a266c0aa98782aab6f7184648634dc806fbc
Okay Padak <kypdk@proton.me>
2025-05-21 22:17:48 +0300

    güncel

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue(),tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_SERVER_URL,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
